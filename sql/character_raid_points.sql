67e02ed5d7455de5fd906c7630d6fc4b31890f1c
Crevan <a.siverskii@gmail.com>
2022-10-29 19:39:09 +0200

    [add] sql script for creating character_raid_points table

---
+++ sql/character_raid_points.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `character_raid_points`
+(
+    `char_id` INT UNSIGNED NOT NULL DEFAULT 0,
+    `boss_id` INT UNSIGNED NOT NULL DEFAULT 0,
+    `points`  INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`char_id`, `boss_id`)
+);
