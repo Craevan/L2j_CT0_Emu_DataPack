fb5412a37b548a5adf7da8b477a6ee9965d9e665
Crevan <a.siverskii@gmail.com>
2022-11-26 14:43:38 +0100

    [add] sql script for creating seven_signs table

---
+++ sql/seven_signs.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `seven_signs`
+(
+    `char_obj_id`          INT        NOT NULL DEFAULT '0',
+    `cabal`                VARCHAR(8) NOT NULL DEFAULT 'NORMAL',
+    `seal`                 VARCHAR(8) NOT NULL DEFAULT 'NONE',
+    `red_stones`           INT        NOT NULL DEFAULT '0',
+    `green_stones`         INT        NOT NULL DEFAULT '0',
+    `blue_stones`          INT        NOT NULL DEFAULT '0',
+    `ancient_adena_amount` DECIMAL    NOT NULL DEFAULT '0',
+    `contribution_score`   DECIMAL    NOT NULL DEFAULT '0',
+    PRIMARY KEY (`char_obj_id`)
+);
