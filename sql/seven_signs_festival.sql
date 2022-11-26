e8487950b69617182e80933c687a485824fe8fb5
Crevan <a.siverskii@gmail.com>
2022-11-26 14:43:53 +0100

    [add] sql script for creating seven_signs_festival table

---
+++ sql/seven_signs_festival.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `seven_signs_festival`
+(
+    `festivalId` int          NOT NULL DEFAULT '0',
+    `cabal`      varchar(4)   NOT NULL DEFAULT '',
+    `cycle`      int          NOT NULL DEFAULT '0',
+    `date`       bigint                DEFAULT '0',
+    `score`      int          NOT NULL DEFAULT '0',
+    `members`    varchar(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`festivalId`, `cabal`, `cycle`)
+);
+
+INSERT IGNORE INTO `seven_signs_festival`
+VALUES (0, "DAWN", 1, 0, 0, ""),
+       (1, "DAWN", 1, 0, 0, ""),
+       (2, "DAWN", 1, 0, 0, ""),
+       (3, "DAWN", 1, 0, 0, ""),
+       (4, "DAWN", 1, 0, 0, ""),
+       (0, "DUSK", 1, 0, 0, ""),
+       (1, "DUSK", 1, 0, 0, ""),
+       (2, "DUSK", 1, 0, 0, ""),
+       (3, "DUSK", 1, 0, 0, ""),
+       (4, "DUSK", 1, 0, 0, "");
